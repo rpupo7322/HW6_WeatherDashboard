ad85e88bc739aba01c276a403ca7447468de5cf2
Rob Pupo <pupo.robert@gmail.com>
2021-05-03 14:08:04 -0400

    initializing with current location, API link not working yet though

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 var cards = $('.card');
 var current = $('#currentWeather')
-var sidebar = #('#sidenav')
+var sidebar = $('#sidenav')
+var lat;
+var long;
+
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+
+console.log(lat)
+console.log(long)
+
+
+var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={minutely,hourly}&appid={4905034d29ec196fc6fefde21b5e616e}'
+
+console.log(apiURL)
 
 
 
+$.ajax({
+    url: apiURL,
+    method: 'GET',
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) { 
+        console.log('error:', error);
+      });
 
-
+    
 function updateCurrent() {
 
 }
@@ -15,6 +39,27 @@ function updateCards (){
 
     }
 }
+
+
+// initialize the forecast for current location using navigator geolocation
+function success(pos) {
+    var coordinates = pos.coords;
+    lat = coordinates.latitude;
+    long = coordinates.longitude;
+    console.log('Your current position is:');
+    console.log(`Latitude : ${coordinates.latitude}`);
+    console.log(`Longitude: ${coordinates.longitude}`);
+    console.log(`More or less ${coordinates.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    lat = 35.7796
+    long = 78.6382
+  }
+  
+
+
 
 
 cards[0].children[0].textContent = 'seventy four'
