c2c5cf0a0c61ff60a75555cca201fcbf125fcc7b
Rob Pupo <pupo.robert@gmail.com>
2021-05-03 13:28:08 -0400

    some initial js

---
+++ script.js
@@ -0,0 +1,20 @@
+var cards = $('.card');
+var current = $('#currentWeather')
+var sidebar = #('#sidenav')
+
+
+
+
+
+function updateCurrent() {
+
+}
+
+function updateCards (){
+    for(i=0;i<cards.length;i++) {
+
+    }
+}
+
+
+cards[0].children[0].textContent = 'seventy four'
